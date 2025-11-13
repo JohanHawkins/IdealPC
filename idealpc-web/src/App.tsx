879887291f17e8d5084890c3742438f4f03b3962
Johan Hawkins <johanhawkins14@gmail.com>
2025-11-12 20:02:30 -0500

    Error y Solucion
    Problema en instalacion de los paquetes

---
+++ idealpc-web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container className="text-center mt-5">
+      <h1 className="mb-4">💻 IdealPC</h1>
+      <p>Proyecto inicial con Vite + React + Bootstrap</p>
+      <Button variant="primary">Probar Bootstrap</Button>
+    </Container>
+  );
+}
+
+export default App;
